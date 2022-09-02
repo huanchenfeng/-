199ab181f66691548b114ba0b106e6c0aca6c53d
meng <2733828240@qq.com>
2022-09-02 23:10:07 +0800

    lanjieqi

---
+++ src/main/java/com/meng/reggie/filter/LoginCheckFilter.java
@@ -0,0 +1,53 @@
+package com.meng.reggie.filter;
+
+import com.alibaba.fastjson.JSON;
+import com.meng.reggie.common.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.AntPathMatcher;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(filterName = "loginCheckFilter",urlPatterns = "/*")
+@Slf4j
+public class LoginCheckFilter implements Filter {
+    public static final AntPathMatcher PATH_MATCHER=new AntPathMatcher();
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request= (HttpServletRequest) servletRequest;
+        HttpServletResponse response= (HttpServletResponse) servletResponse;
+        String requestURI =request.getRequestURI();
+        //不需要处理的请求路径
+        String[] urls=new String[]{
+                "employee/login",
+                "employee/logout",
+                "backend/**",
+                "front/**"
+        };
+        //判断本次请求是否需要处理
+        boolean check=check(urls,requestURI);
+
+        //判断是否需要处理
+        if (check){
+            filterChain.doFilter(request,response);
+            return;
+        }
+        //是否登录成功
+        if (request.getSession().getAttribute("employee")!=null){
+            filterChain.doFilter(request,response);
+            return;
+        }
+        response.getWriter().write(JSON.toJSONString(R.error("NOTLOGIN")));
+    }
+    public boolean check (String[] urls,String requestURI){
+        for (String url : urls) {
+            if(PATH_MATCHER.match(url,requestURI)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- src/main/java/com/meng/reggie/reggieApplication.java
+++ src/main/java/com/meng/reggie/reggieApplication.java
@@ -5,9 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @Slf4j
 @SpringBootApplication()
+@ServletComponentScan
 public class reggieApplication {
     public static void main(String[] args) {
         SpringApplication.run(reggieApplication.class,args);
